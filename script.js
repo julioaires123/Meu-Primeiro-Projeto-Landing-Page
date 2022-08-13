c77db2ce5633cbd33541321d246beb6b02e48b26
JÚLIO CARLOS DE OLIVEIRA AIRES <52129925+julioaires123@users.noreply.github.com>
2022-08-13 16:13:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,108 @@
-# Meu-Primeiro-Projeto-Landing-Page
+setInterval(function relog() {
+    let rel = document.getElementById('relogio')
+    let data = new Date()
+    let h = data.getHours()
+    let m = data.getMinutes()
+    let s = data.getSeconds()
+
+    if (h < 10) {
+        h = `0${h}`
+    }
+
+    if (m < 10) {
+        m = `0${m}`
+    }
+
+    if (s < 10) {
+        s = `0${s}`
+    }
+
+    rel.innerHTML = `${h}:${m}:${s}`
+
+
+}, 1000)
+
+
+
+let rel = document.getElementById('dia')
+let dt = new Date()
+let diasem = dt.getDay();
+let dia = dt.getDate()
+let mes = dt.getMonth()
+let ano = dt.getFullYear()
+
+
+
+let meses = new Array("Dezembro", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Julho", "Agosto", "Setembro", "Outubro", "Novembro")
+let semanas = new Array("Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado")
+
+document.write(semanas[diasem] + ", " + dia + " de " + meses[mes] + " de " + ano);
+
+
+
+
+setInterval(function relog2() {
+    let rel = document.getElementById('relogio2')
+    let data = new Date()
+    let h = data.getHours() + 1
+    let m = data.getMinutes()
+    let s = data.getSeconds()
+
+    if (h < 10) {
+        h = `0${h}`
+    }
+
+    if (m < 10) {
+        m = `0${m}`
+    }
+
+    if (s < 10) {
+        s = `0${s}`
+    }
+
+    rel.innerHTML = `${h}:${m}:${s}`
+}, 1000)
+
+setInterval(function relog3() {
+    let rel = document.getElementById('relogio3')
+    let data = new Date()
+    let h = data.getHours() - 1
+    let m = data.getMinutes()
+    let s = data.getSeconds()
+
+    if (h < 10) {
+        h = `0${h}`
+    }
+
+    if (m < 10) {
+        m = `0${m}`
+    }
+
+    if (s < 10) {
+        s = `0${s}`
+    }
+
+    rel.innerHTML = `${h}:${m}:${s}`
+}, 1000)
+
+setInterval(function relog4() {
+    let rel = document.getElementById('relogio4')
+    let data = new Date()
+    let h = data.getHours() - 2
+    let m = data.getMinutes()
+    let s = data.getSeconds()
+
+    if (h < 10) {
+        h = `0${h}`
+    }
+
+    if (m < 10) {
+        m = `0${m}`
+    }
+
+    if (s < 10) {
+        s = `0${s}`
+    }
+
+    rel.innerHTML = `${h}:${m}:${s}`
+}, 1000)
