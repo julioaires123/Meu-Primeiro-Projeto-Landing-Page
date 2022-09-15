e26abcc7eab5ce4a1fa487a4b28c87c551253d49
JÚLIO CARLOS DE OLIVEIRA AIRES <52129925+julioaires123@users.noreply.github.com>
2022-09-15 10:32:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,142 @@
 setInterval(function relog() {
     let rel = document.getElementById('relogio')
-    let data = new Date()
-    let h = data.getHours()
-    let m = data.getMinutes()
-    let s = data.getSeconds() 
+    let data = new Date();
+    let h = data.getHours();
+    let m = data.getMinutes();
+    let s = data.getSeconds() + 30;
+        
+    if (s > 59) {
+
+        m = m + 1;
+    }
+    if (s === 60) {
+
+        s = 00;
+    }
+    if (s === 61) {
+
+        s = 01;
+    }
+    if (s === 62) {
+
+        s = 02;
+    }
+
+    if (s === 63) {
+
+        s = 03;
+    }
+    if (s === 64) {
+
+        s = 04;
+    }
+    if (s === 65) {
+
+        s = 05;
+    }
+    if (s === 66) {
+
+        s = 06;
+    }
+
+    if (s === 67) {
+
+        s = 07;
+    }
+    if (s === 68) {
+
+        s = 08;
+    }
+    if (s === 69) {
+
+        s = 09;
+    }
+
+    if (s === 70) {
+
+        s = 10;
+    }
+    if (s === 71) {
+
+        s = 11;
+    }
+    if (s === 72) {
+
+        s = 12;
+    }
+
+    if (s === 73) {
+
+        s = 13;
+    }
+    if (s === 74) {
+
+        s = 14;
+    }
+    if (s === 75) {
+
+        s = 15;
+    }
+    if (s === 76) {
+
+        s = 16;
+    }
+
+    if (s === 77) {
+
+        s = 17;
+    }
+    if (s === 78) {
+
+        s = 18;
+    }
+    if (s === 79) {
+
+        s = 19;
+    }
+    if (s === 80) {
+
+        s = 20;
+    }
+    if (s === 81) {
+
+        s = 21;
+    }
+    if (s === 82) {
+
+        s = 22;
+    }
+
+    if (s === 83) {
+
+        s = 23;
+    }
+    if (s === 84) {
+
+        s = 24;
+    }
+    if (s === 85) {
+
+        s = 25;
+    }
+    if (s === 86) {
+
+        s = 26;
+    }
+
+    if (s === 87) {
+
+        s = 27;
+    }
+    if (s === 88) {
+
+        s = 28;
+    }
+    if (s === 89) {
+
+        s = 29;
+    }
+
 
     if (h < 10) {
         h = `0${h}`
