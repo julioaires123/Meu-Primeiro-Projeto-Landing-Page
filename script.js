7eb3583e3a7bb4b0aca50fc983534a6dbc452f04
JÚLIO CARLOS DE OLIVEIRA AIRES <52129925+julioaires123@users.noreply.github.com>
2023-07-04 13:56:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// Horário de Brasília
-setInterval(function() {
+setInterval(function relog() {
     let rel = document.getElementById('relogio01');
     let data = new Date();
-    data.setSeconds(data.getSeconds() + 20);
     let h = data.getHours();
     let m = data.getMinutes();
     let s = data.getSeconds();
-
     if (h < 10) {
         h = `0${h}`;
     }
@@ -16,16 +13,16 @@ setInterval(function() {
     if (s < 10) {
         s = `0${s}`;
     }
-
     rel.innerHTML = `${h}:${m}:${s}`;
 }, 1000);
 
-// Demais horários
-function formatTime(data) {
+setInterval(function relog2() {
+    let rel = document.getElementById('relogio2');
+    let data = new Date();
+    data.setHours(data.getHours() + 1);
     let h = data.getHours();
     let m = data.getMinutes();
     let s = data.getSeconds();
-
     if (h < 10) {
         h = `0${h}`;
     }
@@ -35,24 +32,43 @@ function formatTime(data) {
     if (s < 10) {
         s = `0${s}`;
     }
-
-    return `${h}:${m}:${s}`;
-}
-
-setInterval(function() {
-    let rel = document.getElementById('Relogio05');
-    let data = new Date();
-    rel.innerHTML = formatTime(data);
+    rel.innerHTML = `${h}:${m}:${s}`;
 }, 1000);
 
-setInterval(function() {
-    let rel = document.getElementById('date');
+setInterval(function relog3() {
+    let rel = document.getElementById('relogio3');
     let data = new Date();
-    let meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
-    let semanas = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
-    let diaSem = data.getDay();
-    let dia = data.getDate();
-    let mes = data.getMonth();
-    let ano = data.getFullYear();
-    rel.innerHTML = semanas[diaSem] + ", " + dia + " de " + meses[mes] + " de " + ano;
+    data.setHours(data.getHours() - 1);
+    let h = data.getHours();
+    let m = data.getMinutes();
+    let s = data.getSeconds();
+    if (h < 10) {
+        h = `0${h}`;
+    }
+    if (m < 10) {
+        m = `0${m}`;
+    }
+    if (s < 10) {
+        s = `0${s}`;
+    }
+    rel.innerHTML = `${h}:${m}:${s}`;
+}, 1000);
+
+setInterval(function relog4() {
+    let rel = document.getElementById('relogio4');
+    let data = new Date();
+    data.setHours(data.getHours() - 2);
+    let h = data.getHours();
+    let m = data.getMinutes();
+    let s = data.getSeconds();
+    if (h < 10) {
+        h = `0${h}`;
+    }
+    if (m < 10) {
+        m = `0${m}`;
+    }
+    if (s < 10) {
+        s = `0${s}`;
+    }
+    rel.innerHTML = `${h}:${m}:${s}`;
 }, 1000);
