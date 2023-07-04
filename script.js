deaca8bea49975e04a9d53c856a292667e071b0a
JÚLIO CARLOS DE OLIVEIRA AIRES <52129925+julioaires123@users.noreply.github.com>
2023-07-04 16:10:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
         let rel = document.getElementById('relogio01')
         let data = new Date();
         data.setHours(data.getHours() +8);  
-        data.setMinutes(data.getMinutes() +50);      
+        data.setMinutes(data.getMinutes() +49);      
         data.setSeconds(data.getSeconds() +20);
         let h = data.getHours();
         let m = data.getMinutes();
