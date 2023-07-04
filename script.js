9680e403220bdfc286821babc4e5cdf338fe4f99
JÚLIO CARLOS DE OLIVEIRA AIRES <52129925+julioaires123@users.noreply.github.com>
2023-07-04 13:53:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,103 +1,58 @@
-//horário de Brasília////////////////////////////////////////////////////////////////////////////////////////
-        setInterval(function relog() {
-        let rel = document.getElementById('relogio01')
-        let data = new Date();
-        data.setSeconds(data.getSeconds() +20);
-        let h = data.getHours();
-        let m = data.getMinutes();
-        let s = data.getSeconds();
-          if (h < 10) {// coloca um zero antes dos números abaixo de dez: ex: 1: 1: 1 depois ex 01:01:01////////////////////
-           h = `0${h}`
-        }
-        if (m < 10) {
-           m = `0${m}`
-        }  
-        if (s < 10) {
-           s = `0${s}`
-        }
-        // Mostra a hora minutos e segundos na página /////////////////////////////////////////////////////
-            rel.innerHTML = `${h}:${m}:${s}`
-        })
-    //horário de Fernando de Noronha////////////////////////////////////////////////////////////////////////  
-    setInterval(function relog2() {
-        let rel = document.getElementById('relogio2')
-        let data = new Date();
-        data.setSeconds(data.getSeconds() +20);
-        data.setHours(data.getHours() +1);
-        let h = data.getHours() ; 
-        let m = data.getMinutes();
-        let s = data.getSeconds() 
-           if (h < 10) {  // coloca um zero antes dos números abaixo de dez: ex: 1: 1: 1 depois ex 01:01:01////////////////////
-              h = `0${h}`
-           } 
-           if (m < 10) {
-              m = `0${m}`
-           }
-           if (s < 10) {
-              s = `0${s}`
-           }
-           // Mostra a hora minutos e segundos na página /////////////////////////////////////////////////////
-               rel.innerHTML = `${h}:${m}:${s}`
-           })
-    //horário de Amazonas///////////////////////////////////////////////////////////////////////////////////   
-    setInterval(function relog3() {
-        let rel = document.getElementById('relogio3');
-        let data = new Date();
-        data.setSeconds(data.getSeconds() +20);
-        data.setHours(data.getHours() -1);
-        let h = data.getHours() ;
-        let m = data.getMinutes();
-        let s = data.getSeconds(); 
-          if (h < 10) {  // coloca um zero antes dos números abaixo de dez: ex: 1: 1: 1 depois ex 01:01:01////////////////////
-           h = `0${h}`
-        }
-        if (m < 10) {
-           m = `0${m}`
-        }
-        if (s < 10) {
-           s = `0${s}`
-        }
-            rel.innerHTML = `${h}:${m}:${s}`   // Mostra a hora minutos e segundos na página /////////////////////////////////////////////////////
-           })
-    //horário de Acre////////////////////////////////////////////////////////////////////////////////////
-    setInterval(function relog4() {
-        let rel = document.getElementById('relogio4')
-        let data = new Date();
-        data.setSeconds(data.getSeconds() +20);
-        data.setHours(data.getHours() -2);
-        let h = data.getHours();
-        let m = data.getMinutes();
-        let s = data.getSeconds() ;  
-          if (h < 10) { // coloca um zero antes dos números abaixo de dez: ex: 1: 1: 1 depois ex 01:01:01////////////////////
-           h = `0${h}`
-        }
-        if (m < 10) {
-           m = `0${m}`
-        } 
-        if (s < 10) {
-           s = `0${s}`
-        }
-            rel.innerHTML = `${h}:${m}:${s}`  // Mostra a hora minutos e segundos na página /////////////////////////////////////////////////////       
-           })   
-//Dia mês e ano /////////////////////////////////////////////////////////////////////////////////////////////
-let meses = new Array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro");
-let semanas = new Array("Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado");
+// Horário de Brasília
+setInterval(function() {
+    let rel = document.getElementById('relogio01');
+    let data = new Date();
+    data.setSeconds(data.getSeconds() + 20);
+    let h = data.getHours();
+    let m = data.getMinutes();
+    let s = data.getSeconds();
 
-function exibirDataAtual() {
-  let data = new Date();
-  let diasem = data.getDay();
-  let dia = data.getDate();
-  let mes = data.getMonth();
-  let ano = data.getFullYear();
+    if (h < 10) {
+        h = `0${h}`;
+    }
+    if (m < 10) {
+        m = `0${m}`;
+    }
+    if (s < 10) {
+        s = `0${s}`;
+    }
 
-  let elementoData = document.getElementById("data");
-  elementoData.textContent = semanas[diasem] + ", " + dia + " de " + meses[mes] + " de " + ano;
+    rel.innerHTML = `${h}:${m}:${s}`;
+}, 1000);
+
+// Demais horários
+function formatTime(data) {
+    let h = data.getHours();
+    let m = data.getMinutes();
+    let s = data.getSeconds();
+
+    if (h < 10) {
+        h = `0${h}`;
+    }
+    if (m < 10) {
+        m = `0${m}`;
+    }
+    if (s < 10) {
+        s = `0${s}`;
+    }
+
+    return `${h}:${m}:${s}`;
 }
 
-// Executa a função inicialmente
-exibirDataAtual();
+setInterval(function() {
+    let rel = document.getElementById('Relogio05');
+    let data = new Date();
+    rel.innerHTML = formatTime(data);
+}, 1000);
 
-// Atualiza a cada segundo (ajuste o intervalo conforme necessário)
-setInterval(exibirDataAtual, 1000);
-
-
+setInterval(function() {
+    let rel = document.getElementById('date');
+    let data = new Date();
+    let meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+    let semanas = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
+    let diaSem = data.getDay();
+    let dia = data.getDate();
+    let mes = data.getMonth();
+    let ano = data.getFullYear();
+    rel.innerHTML = semanas[diaSem] + ", " + dia + " de " + meses[mes] + " de " + ano;
+}, 1000);
