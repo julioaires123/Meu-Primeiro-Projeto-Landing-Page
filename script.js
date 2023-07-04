3ca6bd5f98c02bce80ebd5ecedc31af958fd43b1
JÚLIO CARLOS DE OLIVEIRA AIRES <52129925+julioaires123@users.noreply.github.com>
2023-07-04 14:05:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -66,12 +66,12 @@ setInterval(function relog3() {
     document.getElementById('data3').innerHTML = `${dia}/${mes}/${ano}`;
 }, 1000);
 
-setInterval(function relog4() {
-    let rel = document.getElementById('relogio4');
+setInterval(function relog() {
+    let rel = document.getElementById('relogio01');
     let data = new Date();
-    data.setHours(data.getHours() - 2);
+    let diaSemana = obterDiaSemanaPorExtenso(data.getDay());
     let dia = data.getDate();
-    let mes = data.getMonth() + 1;
+    let mes = obterNomeMesPorExtenso(data.getMonth());
     let ano = data.getFullYear();
     let h = data.getHours();
     let m = data.getMinutes();
@@ -85,6 +85,39 @@ setInterval(function relog4() {
     if (s < 10) {
         s = `0${s}`;
     }
-    rel.innerHTML = `${h}:${m}:${s}`;
-    document.getElementById('data4').innerHTML = `${dia}/${mes}/${ano}`;
+    rel.innerHTML = `${diaSemana}, ${dia} de ${mes} de ${ano}`;
+    document.getElementById('hora01').innerHTML = `${h}:${m}:${s}`;
 }, 1000);
+
+// Função para obter o nome do dia da semana por extenso
+function obterDiaSemanaPorExtenso(diaSemana) {
+    let diasSemana = [
+        'Domingo',
+        'Segunda-feira',
+        'Terça-feira',
+        'Quarta-feira',
+        'Quinta-feira',
+        'Sexta-feira',
+        'Sábado'
+    ];
+    return diasSemana[diaSemana];
+}
+
+// Função para obter o nome do mês por extenso
+function obterNomeMesPorExtenso(mes) {
+    let meses = [
+        'Janeiro',
+        'Fevereiro',
+        'Março',
+        'Abril',
+        'Maio',
+        'Junho',
+        'Julho',
+        'Agosto',
+        'Setembro',
+        'Outubro',
+        'Novembro',
+        'Dezembro'
+    ];
+    return meses[mes];
+}
