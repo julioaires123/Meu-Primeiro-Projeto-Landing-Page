3da8276f4c75952cfee4da01ac6bf796b7ec3fa9
JÚLIO CARLOS DE OLIVEIRA AIRES <52129925+julioaires123@users.noreply.github.com>
2023-07-04 16:48:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -83,60 +83,24 @@
            })   
 //Dia mês e ano /////////////////////////////////////////////////////////////////////////////////////////////
 // Função para exibir a data atualizada
-function exibirDataAtualizada() {
-  let meses = [
-    "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
-    "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
-  ];
-  let semanas = [
-    "Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira",
-    "Quinta-Feira", "Sexta-Feira", "Sábado"
-  ];
+let meses = new Array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro");
+let semanas = new Array("Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado");
 
+function exibirDataAtual() {
   let data = new Date();
   let diasem = data.getDay();
   let dia = data.getDate();
   let mes = data.getMonth();
   let ano = data.getFullYear();
-
-  // Verifica se é meia-noite (00:00:00)
-  if (data.getHours() === 0 && data.getMinutes() === 0 && data.getSeconds() === 0) {
-    // Incrementa um dia
-    data.setDate(data.getDate() + 1);
-    dia = data.getDate();
-    mes = data.getMonth();
-    ano = data.getFullYear();
-  }
-
-  // Atualiza o conteúdo do elemento com o ID "date"
-  document.getElementById("date").innerHTML = semanas[diasem] + ", " + dia + " de " + meses[mes] + " de " + ano;
+  document.write(semanas[diasem] + ", " + dia + " de " + meses[mes] + " de " + ano);
 }
 
-// Função para atualizar a data a cada segundo
-function atualizarData() {
-  let data = new Date();
-  let horas = data.getHours();
-  let minutos = data.getMinutes();
-  let segundos = data.getSeconds();
+// Executa a função inicialmente
+exibirDataAtual();
 
-  // Verifica se é meia-noite (00:00:00)
-  if (horas === 0 && minutos === 0 && segundos === 0) {
-    exibirDataAtualizada();
+// Atualiza a cada segundo (ajuste o intervalo conforme necessário)
+setInterval(exibirDataAtual, 1000);
 
-    // Define o próximo intervalo de atualização para o próximo dia
-    let proximaAtualizacao = new Date();
-    proximaAtualizacao.setDate(proximaAtualizacao.getDate() + 1);
-    proximaAtualizacao.setHours(0);
-    proximaAtualizacao.setMinutes(0);
-    proximaAtualizacao.setSeconds(0);
-
-    let tempoAteProximaAtualizacao = proximaAtualizacao.getTime() - data.getTime();
-
-    setTimeout(atualizarData, tempoAteProximaAtualizacao);
-  } else {
-    setTimeout(atualizarData, 1000); // Chama a função novamente após 1 segundo
-  }
-}
 
 // Chama a função para exibir a data atualizada
 exibirDataAtualizada();
