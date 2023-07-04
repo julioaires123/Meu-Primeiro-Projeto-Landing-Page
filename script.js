c853eeeaa48f81996f929c617f141ddb94f86b01
JÚLIO CARLOS DE OLIVEIRA AIRES <52129925+julioaires123@users.noreply.github.com>
2023-07-04 16:51:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -82,32 +82,62 @@
 //Dia mês e ano /////////////////////////////////////////////////////////////////////////////////////////////
 // Função para exibir a data atualizada
 function exibirDataAtualizada() {
-    let meses = [
-        "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
-        "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
-    ];
-    let semanas = [
-        "Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira",
-        "Quinta-Feira", "Sexta-Feira", "Sábado"
-    ];
+  let meses = [
+    "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+    "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+  ];
+  let semanas = [
+    "Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira",
+    "Quinta-Feira", "Sexta-Feira", "Sábado"
+  ];
 
-    let data = new Date();
-    let diasem = data.getDay();
-    let dia = data.getDate();
-    let mes = data.getMonth();
-    let ano = data.getFullYear();
+  let data = new Date();
+  let diasem = data.getDay();
+  let dia = data.getDate();
+  let mes = data.getMonth();
+  let ano = data.getFullYear();
 
-    // Atualiza o conteúdo do elemento com o ID "date"
-    document.getElementById("date").innerHTML = semanas[diasem] + ", " + dia + " de " + meses[mes] + " de " + ano;
+  // Verifica se é meia-noite (00:00:00)
+  if (data.getHours() === 0 && data.getMinutes() === 0 && data.getSeconds() === 0) {
+    // Incrementa um dia
+    data.setDate(data.getDate() + 1);
+    dia = data.getDate();
+    mes = data.getMonth();
+    ano = data.getFullYear();
+  }
+
+  // Atualiza o conteúdo do elemento com o ID "date"
+  document.getElementById("date").innerHTML = semanas[diasem] + ", " + dia + " de " + meses[mes] + " de " + ano;
+}
+
+// Função para atualizar a data a cada segundo
+function atualizarData() {
+  let data = new Date();
+  let horas = data.getHours();
+  let minutos = data.getMinutes();
+  let segundos = data.getSeconds();
+
+  // Verifica se é meia-noite (00:00:00)
+  if (horas === 0 && minutos === 0 && segundos === 0) {
+    exibirDataAtualizada();
+
+    // Define o próximo intervalo de atualização para o próximo dia
+    let proximaAtualizacao = new Date();
+    proximaAtualizacao.setDate(proximaAtualizacao.getDate() + 1);
+    proximaAtualizacao.setHours(0);
+    proximaAtualizacao.setMinutes(0);
+    proximaAtualizacao.setSeconds(0);
+
+    let tempoAteProximaAtualizacao = proximaAtualizacao.getTime() - data.getTime();
+
+    setTimeout(atualizarData, tempoAteProximaAtualizacao);
+  } else {
+    setTimeout(atualizarData, 1000); // Chama a função novamente após 1 segundo
+  }
 }
 
 // Chama a função para exibir a data atualizada
 exibirDataAtualizada();
 
-// Atualiza a data à meia-noite
-setInterval(function() {
-    let data = new Date();
-    if (data.getHours() === 00 && data.getMinutes() === 00 && data.getSeconds() === 00) {
-        exibirDataAtualizada();
-    }
-}, 1000);
+// Atualiza a data a cada segundo
+atualizarData();
