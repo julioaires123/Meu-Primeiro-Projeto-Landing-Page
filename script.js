b3643282c1dfc288e29589f1537c8f76d1bd1597
JÚLIO CARLOS DE OLIVEIRA AIRES <52129925+julioaires123@users.noreply.github.com>
2023-07-05 01:00:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -123,7 +123,7 @@ function atualizarData() {
 
     // Define o próximo intervalo de atualização para o próximo dia
     let proximaAtualizacao = new Date();
-    proximaAtualizacao.setDate(proximaAtualizacao.getDate() + 1);
+    proximaAtualizacao.setDate(proximaAtualizacao.getDate();
     proximaAtualizacao.setHours(0);
     proximaAtualizacao.setMinutes(0);
     proximaAtualizacao.setSeconds(0);
