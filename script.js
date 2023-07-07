cc4375b60f79f97f66b98153a64ef446e21503a2
JÚLIO CARLOS DE OLIVEIRA AIRES <52129925+julioaires123@users.noreply.github.com>
2023-07-07 09:56:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -22,29 +22,31 @@ function obterHoraInternet(timezone) {
 // Função para exibir a hora de uma região específica
 function exibirHora(timezone, elementId) {
   let rel = document.getElementById(elementId);
-  obterHoraInternet(timezone).then((dateTime) => {
-    let h = dateTime.getHours();
-    let m = dateTime.getMinutes();
-    let s = dateTime.getSeconds();
+  obterHoraInternet(timezone)
+    .then((dateTime) => {
+      let h = dateTime.getHours();
+      let m = dateTime.getMinutes();
+      let s = dateTime.getSeconds();
 
-    if (h < 10) {
-      h = `0${h}`;
-    }
-    if (m < 10) {
-      m = `0${m}`;
-    }
-    if (s < 10) {
-      s = `0${s}`;
-    }
+      if (h < 10) {
+        h = `0${h}`;
+      }
+      if (m < 10) {
+        m = `0${m}`;
+      }
+      if (s < 10) {
+        s = `0${s}`;
+      }
 
-    rel.innerHTML = `${h}:${m}:${s}`;
-  }).catch((error) => {
-    console.error(error);
-  });
+      rel.innerHTML = `${h}:${m}:${s}`;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
 // Função para exibir a data atualizada
-function exibirDataAtualizada(dateTime) {
+function exibirDataAtualizada() {
   let meses = [
     "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
     "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
@@ -54,31 +56,14 @@ function exibirDataAtualizada(dateTime) {
     "Quinta-Feira", "Sexta-Feira", "Sábado"
   ];
 
-  let diasem = dateTime.getDay();
-  let dia = dateTime.getDate();
-  let mes = dateTime.getMonth();
-  let ano = dateTime.getFullYear();
-
-  // Verifica se é meia-noite (00:00:00)
-  if (dateTime.getHours() === 0 && dateTime.getMinutes() === 0 && dateTime.getSeconds() === 0) {
-    // Incrementa um dia
-    dateTime.setDate(dateTime.getDate() + 1);
-    dia = dateTime.getDate();
-    mes = dateTime.getMonth();
-    ano = dateTime.getFullYear();
-  }
+  let data = new Date();
+  let diasem = data.getDay();
+  let dia = data.getDate();
+  let mes = data.getMonth();
+  let ano = data.getFullYear();
 
   // Atualiza o conteúdo do elemento com o ID "date"
   document.getElementById("date").innerHTML = semanas[diasem] + ", " + dia + " de " + meses[mes] + " de " + ano;
-}
-
-// Função para atualizar a data à meia-noite
-function atualizarData() {
-  obterHoraInternet("America/Sao_Paulo").then((dateTime) => {
-    exibirDataAtualizada(dateTime);
-  }).catch((error) => {
-    console.error(error);
-  });
 }
 
 // Chama as funções para exibir os horários das diferentes regiões do Brasil
@@ -95,6 +80,8 @@ setInterval(() => {
   exibirHora("America/Rio_Branco", "relogio4");
 }, 1000);
 
-// Chama a função para atualizar a data à meia-noite
-atualizarData();
-setInterval(atualizarData, 1000);
+// Chama a função para exibir a data atualizada
+exibirDataAtualizada();
+
+// Atualiza a data a cada segundo
+setInterval(exibirDataAtualizada, 1000);
