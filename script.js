c8e9a97f9fb791e206426597d08e5a2a25922dce
JÚLIO CARLOS DE OLIVEIRA AIRES <52129925+julioaires123@users.noreply.github.com>
2023-11-16 23:15:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
         setInterval(function relog() {
         let rel = document.getElementById('relogio01')
         let data = new Date();
-        data.setSeconds(data.getSeconds() +5);
-        let h = data.getHours();
+        data.setSeconds(data.getSeconds() +00);
+        let h = data.getHours() +8);;
         let m = data.getMinutes();
         let s = data.getSeconds();
           if (h < 10) {// coloca um zero antes dos números abaixo de dez: ex: 1: 1: 1 depois ex 01:01:01////////////////////
